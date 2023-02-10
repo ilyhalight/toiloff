c46202a296a3f82cac65e1fe3cbffc77c1f60341
Toil <toil.official@yandex.com>
2023-02-11 02:09:46 +0300

    added new navbar

---
+++ components/Header.vue
@@ -0,0 +1,22 @@
+<script setup>
+  import DropDownLocales from './DropDowns/DropDownLocales.vue';
+  import ThemesButton from './Buttons/ThemesButton.vue';
+</script>
+
+<template>
+  <header class="header">
+    <NuxtLink to="/" class="logo">
+      {{ '</' }}<span>ToilDev</span>>
+    </NuxtLink>
+    <nav class="navbar">
+      <NuxtLink class="navbar-item" to="/about"> {{ $t('About me') }} </NuxtLink>
+      <NuxtLink class="navbar-item" to="/donate"> {{ $t('Donate') }} </NuxtLink>
+      <NuxtLink class="navbar-item" to="/blog"> {{ $t('Blog') }} </NuxtLink>
+      <NuxtLink class="navbar-item" to="/social"> {{ $t('Social') }} </NuxtLink>
+    </nav>
+    <div class="navbar">
+      <themes-button class="navbar-item" />
+      <drop-down-locales class="navbar-item" />
+    </div>
+  </header>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+const { locale } = useI18n();
+</script>
+
+<template>
+  <Header/>
+  <main>
+    <h1>{{ $t('Hello') }}</h1>
+    <p>{{ $t('Welcome to my website') }}</p>
+    <p>{{ $t('Current locale') }}: {{ locale }}</p>
+    <p>{{ $t('Current theme') }}: {{ $colorMode.value }}</p>
+  </main>
+</template>
