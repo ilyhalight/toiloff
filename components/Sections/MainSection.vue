a93d842de25dc8fbe1f60650e3fff83384c14406
Toil <toil.official@yandex.com>
2023-02-12 14:16:32 +0300

    sections placed in separate component

---
+++ components/Sections/MainSection.vue
@@ -0,0 +1,54 @@
+<script setup>
+  import { ModalsContainer, useModal, useModalSlot } from 'vue-final-modal';
+  import ModalFull from '~/components/Modals/ModalFull.vue';
+  import ModalImageView from '~/components/Modals/ModalImageView.vue';
+
+  const props = defineProps({
+    items: {
+      type: Array,
+      required: true,
+    },
+  });
+
+  const imageLink = ref('');
+  const imageAlt = ref('');
+
+  const { open, close } = useModal({
+    component: ModalFull,
+    attrs: {
+      title: '',
+    },
+    slots: {
+      default: useModalSlot({
+        component: ModalImageView,
+        attrs: {
+          src: imageLink,
+          alt: imageAlt,
+        }
+      })
+    },
+  });
+
+</script>
+
+<template>
+  <section class="sections">
+    <h1 class="title">
+      <slot name="title"></slot>
+    </h1>
+    <p class="subtitle">
+      <slot name="subtitle"></slot>
+    </p>
+    <div class="sections-items" v-if="items.length">
+      <div class="section-item" v-for="item in items" :key="item.id">
+        <a class="section-title" :href="item.href" target="_blank">{{ item.name }}</a>
+        <div class="tags">
+          <p class="tag" v-for="tag in item.tags" :key="tag" :style="'background: ' + tag.color">{{ tag.name }}</p>
+        </div>
+        <p class="section-description">{{ $t(item.desc) }}.</p>
+        <nuxt-img class="section-image" :src="item.image" :alt="item.name" quality="80" loading="lazy" @click="imageLink = item.image; imageAlt = item.name; open()"/>
+      </div>
+    </div>
+  </section>
+  <ModalsContainer />
+</template>
