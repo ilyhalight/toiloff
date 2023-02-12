acb2601cdb56067e5461051a54d32ce6e77407fe
Toil <toil.official@yandex.com>
2023-02-12 06:09:57 +0300

    added default component for modal on fullscreen

---
+++ components/Modals/ModalFull.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { VueFinalModal } from 'vue-final-modal'
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: false,
+  },
+});
+</script>
+
+<template>
+  <VueFinalModal
+    class="modal"
+    content-class="modal-content-full"
+  >
+    <h1 class="title">
+      {{ title }}
+    </h1>
+    <slot />
+  </VueFinalModal>
+</template>
