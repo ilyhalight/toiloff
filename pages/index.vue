dd2cb2b4212ec21c511eceb3018db9591fa05177
Toil <toil.official@yandex.com>
2023-02-12 14:18:31 +0300

    fixed visibility of texts in section

--- pages/index.vue
+++ pages/index.vue
@@ -18,12 +18,20 @@
       {{ $t('On this site, you can find some information about me, my social networks, as well as about my projects') }}.
     </h2>
     <main-section :items="projects">
-      <h1 class="title">{{ $t('My OpenSource Projects') }}</h1>
-      <p class="subtitle">{{ $t('A little below you can see some of my open source projects') }}.</p>
+      <template #title>
+        <h1 class="title">{{ $t('My OpenSource Projects') }}</h1>
+      </template>
+      <template #subtitle>
+        <p class="subtitle">{{ $t('A little below you can see some of my open source projects') }}.</p>
+      </template>
     </main-section>
     <main-section :items="contributed">
-      <h1 class="title">{{ $t('Contributed') }}</h1>
-      <p class="subtitle">{{ $t('A little below you can see the projects to which I have made any contribution') }}.</p>
+      <template #title>
+        <h1 class="title">{{ $t('Contributed') }}</h1>
+      </template>
+      <template #subtitle>
+        <p class="subtitle">{{ $t('A little below you can see the projects to which I have made any contribution') }}.</p>
+      </template>
     </main-section>
     <!-- <p>{{ $t('Current locale') }}: {{ locale }}</p>
     <p>{{ $t('Current theme') }}: {{ $colorMode.value }}</p> -->
