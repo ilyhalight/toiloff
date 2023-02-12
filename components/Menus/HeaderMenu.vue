7682baf1bd7cd0ae7a8db3ec1e16769aa2218be5
Toil <toil.official@yandex.com>
2023-02-12 06:08:17 +0300

    added header menu

---
+++ components/Menus/HeaderMenu.vue
@@ -0,0 +1,21 @@
+<script setup>
+  import Menu from '~/components/Menus/Menu.vue';
+  import ThemesButton from '~/components/Buttons/ThemesButton.vue';
+  import LocalesMenu from '~/components/Menus/LocalesMenu.vue';
+</script>
+
+<template>
+  <Menu>
+    <template #icon>
+      <Icon class="big-icon" name="gg:menu" />
+    </template>
+    <template #content>
+      <NuxtLink class="menu-item" to="/about"> {{ $t('About me') }} </NuxtLink>
+      <NuxtLink class="menu-item" to="/donate"> {{ $t('Donate') }} </NuxtLink>
+      <NuxtLink class="menu-item" to="/blog"> {{ $t('Blog') }} </NuxtLink>
+      <NuxtLink class="menu-item" to="/social"> {{ $t('Social') }} </NuxtLink>
+      <themes-button class="menu-item" />
+      <locales-menu class="menu-item" />
+    </template>
+  </Menu>
+</template>
