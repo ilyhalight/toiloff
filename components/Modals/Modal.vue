48cebce68b2dd536cf4d4acaa31d4fbe97cac922
Toil <toil.official@yandex.com>
2023-02-12 06:09:24 +0300

    temporary example for creating modals

---
+++ components/Modals/Modal.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ModalsContainer, useModal } from 'vue-final-modal';
+
+import ModalConfirm from '~/components/Modals/ModalConfirm.vue';
+const { open, close } = useModal({
+  component: ModalConfirm,
+  attrs: {
+    title: 'Hello World!',
+    onConfirm() {
+      close()
+    },
+  },
+  slots: {
+    default: '<p>UseModal: The content of the modal</p>',
+  },
+});
+</script>
+
+<template>
+  <ModalsContainer />
+</template>
