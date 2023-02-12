61aeef4173fef24da98fca7c0b5ac6de001a6bd2
Toil <toil.official@yandex.com>
2023-02-12 14:17:10 +0300

    changed name of section components

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import MainSections from '~/components/Sections/MainSections.vue';
+  import MainSection from '~/components/Sections/MainSection.vue';
 
   import projects from "~/data/projects.json";
   import contributed from "~/data/contributed.json";
@@ -17,14 +17,14 @@
     <h2 class="subtext-attractive">
       {{ $t('On this site, you can find some information about me, my social networks, as well as about my projects') }}.
     </h2>
-    <main-sections :items="projects">
+    <main-section :items="projects">
       <h1 class="title">{{ $t('My OpenSource Projects') }}</h1>
       <p class="subtitle">{{ $t('A little below you can see some of my open source projects') }}.</p>
-    </main-sections>
-    <main-sections :items="contributed">
+    </main-section>
+    <main-section :items="contributed">
       <h1 class="title">{{ $t('Contributed') }}</h1>
       <p class="subtitle">{{ $t('A little below you can see the projects to which I have made any contribution') }}.</p>
-    </main-sections>
+    </main-section>
     <!-- <p>{{ $t('Current locale') }}: {{ locale }}</p>
     <p>{{ $t('Current theme') }}: {{ $colorMode.value }}</p> -->
   </main>
