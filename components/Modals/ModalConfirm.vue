5c3ee86f593b5b15891508631975eef302718c7d
Toil <toil.official@yandex.com>
2023-02-12 06:09:45 +0300

    added default component for modal with confirm button

---
+++ components/Modals/ModalConfirm.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { VueFinalModal } from 'vue-final-modal'
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: false,
+  },
+})
+
+const emit = defineEmits(['confirm']);
+</script>
+
+<template>
+  <VueFinalModal
+    class="modal"
+    content-class="modal-content"
+  >
+    <h1 class="title">
+      {{ title }}
+    </h1>
+    <slot />
+    <button class="button" @click="emit('confirm')">
+      {{ $t('Confirm') }}
+    </button>
+  </VueFinalModal>
+</template>
