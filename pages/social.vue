9e0c752eabd60614e6f092d59a0666ee23245a8f
Toil <toil.official@yandex.com>
2023-02-16 23:04:33 +0300

    added social page

---
+++ pages/social.vue
@@ -0,0 +1,35 @@
+<script setup>
+  import VisualLinksSection from '~/components/Sections/VisualLinksSection.vue';
+  import socials from "~/data/socials.json";
+  
+  useHead({
+    title: "Соц. сети · Toiloff",
+    meta: [
+      {
+        name: "og:title",
+        content: "Соц. сети - Toiloff"
+      },
+    ]
+  });
+</script>
+
+<template>
+  <Header/>
+  <main class="wrapper">
+    <h1 class="text-attractive center">
+      {{ $t('Social network') }}
+    </h1>
+    <h2 class="subtext-attractive center">
+      {{ $t('On this page you can see some information about my social networks') }}.
+    </h2>
+    <visual-links-section :items="socials">
+      <template #title>
+        <h1 class="title text-gradient">{{ $t('Social network') }}</h1>
+      </template>
+      <template #subtitle>
+        <p class="subtitle">{{ $t('Socials, Programming, Forums, Games and etc') }}.</p>
+      </template>
+    </visual-links-section>
+  </main>
+  <Footer/>
+</template>
