0714377f9d09642e096cd3a2bb41013b40b1a45c
Toil <toil.official@yandex.com>
2023-02-17 01:07:12 +0300

    added donate page

---
+++ pages/donate.vue
@@ -0,0 +1,28 @@
+<script setup>
+  import DonateSection from '~/components/Sections/DonateSection.vue';
+  import donate from "~/data/donate.json";
+  
+  useHead({
+    title: "Донат · Toiloff",
+    meta: [
+      {
+        name: "og:title",
+        content: "Донат - Toiloff"
+      },
+    ]
+  });
+</script>
+
+<template>
+  <Header/>
+  <main class="wrapper">
+    <h1 class="text-attractive center">
+      {{ $t('Donate') }}
+    </h1>
+    <h2 class="subtext-attractive center">
+      {{ $t('On this page you can see a little information about the opportunity to donate to me') }}.
+    </h2>
+    <donate-section :items="donate"/>
+  </main>
+  <Footer/>
+</template>
