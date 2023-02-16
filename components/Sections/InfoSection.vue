1be5a2bf12871a0679859cc427bb2afe100f346f
Toil <toil.official@yandex.com>
2023-02-16 03:17:20 +0300

    added info section component

---
+++ components/Sections/InfoSection.vue
@@ -0,0 +1,23 @@
+<script setup>
+  const props = defineProps({
+    items: {
+      type: Array,
+      required: true,
+    },
+  });
+
+</script>
+
+<template>
+  <section class="info-section" v-if="items.length">
+    <div class="info-column" v-for="item in items" :key="item[0].id">
+      <div class="info-group" v-for="info in item" :key="info.id">
+        <Icon class="big-icon" :name="info.icon" />
+        <div class="info-text-group">
+          <p class="info-text">{{ $t(info.name) }}:</p>
+          <p class="info-text">{{ $t(info.value) }}</p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
