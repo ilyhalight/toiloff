6b5ee2db701a98b736f0aa2069d91f7f1d334853
unknown <toil.official@yandex.com>
2023-09-20 22:23:45 +0300

    copy modal sepated from main modal

---
+++ components/Modals/ModalCopy.vue
@@ -0,0 +1,17 @@
+<script setup>
+  import CopyToClipboard from '~/components/Buttons/CopyToClipboard.vue';
+
+  const props = defineProps({
+    text: {
+      type: String,
+      required: true,
+    }
+  });
+</script>
+
+<template>
+  <div class="modal-text-block">
+    <p class="modal-text">{{ text }}</p>
+    <copy-to-clipboard :text="text"/>
+  </div>
+</template>
