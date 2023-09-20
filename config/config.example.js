43fc39bc7959774768614f3adaf0e44ba8a633a7
unknown <toil.official@yandex.com>
2023-09-20 22:25:24 +0300

    added config seo params

---
+++ config/config.example.js
@@ -0,0 +1,11 @@
+const config = () => {
+  return {
+    baseUrl: 'http://localhost:3000', // Default url of site for i18n
+    siteName: 'Toiloff',
+    siteDesc: 'Toiloff - это мой личный сайт с небольшим количеством информации обо мне, моих социальных сетях и моих проектах.',
+    personName: 'Toil',
+    titleSeparator: '·',
+  }
+}
+
+export default config();
