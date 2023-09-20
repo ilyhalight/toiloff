060532abee5a44e6dc587dc64b3474b43282e6e7
unknown <toil.official@yandex.com>
2023-09-20 22:24:35 +0300

    added support of mail links, added support of custom icons

--- components/Sections/VisualSection.vue
+++ components/Sections/VisualSection.vue
@@ -10,13 +10,21 @@
 
 <template>
   <section class="visual-section" v-if="items">
-    <h1 class="title center text-gradient">{{ $t('Skills') }}</h1>
-    <p class="subtitle center">{{ $t('Databases and Algorithms, Frontend Development, Backend Development, Database Management') }}.</p>
+    <h1 class="title center text-gradient">{{ $t('content.pages.about.skills.title') }}</h1>
+    <p class="subtitle center">{{ $t('content.pages.about.skills.desc') }}</p>
     <div class="visual-group" v-for="group of Object.entries(items)" :key="group[0]">
       <p class="visual-group-title">{{ $t(group[0]) }}</p>
       <div class="visual-items">
         <div class="visual-item" v-for="visual in group[1]" :key="visual.name">
-          <Icon :name="visual.icon"/>
+          <template v-if="visual.icon_type === 'local'">
+            <nuxt-icon :name="visual.icon" />
+          </template>
+          <template v-else-if="visual.icon_type === 'local-color'">
+            <nuxt-icon :name="visual.icon" filled/>
+          </template>
+          <template v-else>
+            <Icon :name="visual.icon"/>
+          </template>
           <p>{{ visual.name }}</p>
         </div>
       </div>
