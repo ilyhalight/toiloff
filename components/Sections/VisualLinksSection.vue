e82e81c3eb892e83080f9f4a39133789bea68d16
unknown <toil.official@yandex.com>
2023-11-19 01:28:24 +0300

    updated link/modal struct

--- components/Sections/VisualLinksSection.vue
+++ components/Sections/VisualLinksSection.vue
@@ -3,12 +3,17 @@
   import ModalFull from '~/components/Modals/ModalFull.vue';
   import ModalCopy from '~/components/Modals/ModalCopy.vue';
 
-  const donateName = ref('');
-  const donateValue = ref('');
+  const visualLinkTypes = [
+    'link',
+    'mail',
+    'modal'
+  ]
+  const visualName = ref('');
+  const visualValue = ref('');
   const { open, close } = useModal({
     component: ModalFull,
     attrs: {
-      title: donateName,
+      title: visualName,
       onClose() {
         close();
       },
@@ -17,7 +22,7 @@
       default: useModalSlot({
         component: ModalCopy,
         attrs: {
-          text: donateValue,
+          text: visualValue,
         }
       }),
     }
@@ -30,6 +35,16 @@
     },
   });
 
+  function modalLinkCallback(visualItem) {
+    if (visualItem.action_type === "modal") {
+      this.visualName = visualItem.name;
+      this.visualValue = visualItem.content;
+      this.open();
+    }
+
+    return;
+  }
+
 </script>
 
 <template>
@@ -39,24 +54,40 @@
     <div class="visual-group" v-for="group of Object.entries(items)" :key="group[0]">
       <p class="visual-group-title">{{ $t(group[0]) }}</p>
       <div class="visual-items">
-        <div class="visual-item" v-for="visual in group[1]" :key="visual.name">
-          <template v-if="visual.icon_type === 'local'">
-            <nuxt-icon :name="visual.icon" />
-          </template>
-          <template v-else-if="visual.icon_type === 'local-color'">
-            <nuxt-icon :name="visual.icon" filled/>
-          </template>
-          <template v-else>
-            <Icon :name="visual.icon"/>
-          </template>
-          <a
+        <template v-for="visual in group[1]" :key="visual.name">
+          <a v-if="visualLinkTypes.includes(visual.action_type)"
+            class="visual-item visual-link"
             :href="(visual.action_type === 'link' || visual.action_type === 'mail') ? visual.content : '#'"
-            :target="visual.action_type === 'link' ? '_blank' : '_self'"
-            @click="visual.action_type === 'modal' ? (donateName = visual.name, donateValue = visual.content, open()) : ''"
-          >
-            {{ visual.name }}
+            @click="modalLinkCallback(visual)"
+            >
+            <template v-if="visual.icon_type === 'local'">
+              <nuxt-icon :name="visual.icon" />
+            </template>
+            <template v-else-if="visual.icon_type === 'local-color'">
+              <nuxt-icon :name="visual.icon" filled/>
+            </template>
+            <template v-else>
+              <Icon :name="visual.icon"/>
+            </template>
+            <p>
+              {{ visual.name }}
+            </p>
           </a>
-        </div>
+          <div v-else class="visual-item">
+            <template v-if="visual.icon_type === 'local'">
+              <nuxt-icon :name="visual.icon" />
+            </template>
+            <template v-else-if="visual.icon_type === 'local-color'">
+              <nuxt-icon :name="visual.icon" filled/>
+            </template>
+            <template v-else>
+              <Icon :name="visual.icon"/>
+            </template>
+            <p>
+              {{ visual.name }}
+            </p>
+          </div>
+        </template>
       </div>
     </div>
   </section>
