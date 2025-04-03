281c349cf90bcf7279a0534d8646eac309acb4a6
Toil <62353659+ilyhalight@users.noreply.github.com>
2025-04-03 19:02:39 +0300

    chore: added actual info notice

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,10 +1,42 @@
 <script setup>
+  import { h } from "vue";
+  import { ModalsContainer, useModal, useModalSlot } from 'vue-final-modal';
+  import ModalFull from '~/components/Modals/ModalFull.vue';
+
+  const imageLink = ref('');
+  const imageAlt = ref('');
+  const { t } = useI18n();
+
+  const { open, close } = useModal({
+    component: ModalFull,
+    attrs: {
+      title: t("footer.actual-info.title"),
+      onClose() {
+        close();
+      },
+    },
+    slots: {
+      default: (() => h("p", { style: { fontSize: "20px", color: "#f7f7ff" } }, t("footer.actual-info.desc")))
+    },
+  });
 
 </script>
 
 <template>
   <footer class="footer">
-    <p>{{ $t('footer.made_with_love') }}</p>
-    <NuxtLink to="/">Toil</NuxtLink>
+    <div class="footer-info">
+      <a href="#" @click.prevent="open()">{{ $t("footer.actual-info.title") }}</a>
+    </div>
+    <div class="footer-creds">
+      <p>{{ $t("footer.made_with_love") }}</p>
+      <NuxtLink to="/">Toil</NuxtLink>
+    </div>
   </footer>
 </template>
+
+<style>
+  .footer-creds {
+    display: flex;
+    align-items: center;
+  }
+</style>
