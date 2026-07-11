350587d68e67ee2407e5f13384fd666bc5f70fd7
Toil <62353659+ilyhalight@users.noreply.github.com>
2026-07-11 14:09:38 +0300

    feat: added real stats api

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,7 +1,9 @@
 import { AuthRouteAPI } from "./auth";
 import { GuestbookRouteAPI } from "./guestbook";
+import { StatsRouteAPI } from "./stats";
 
 export const BackendAPI = {
   guestbook: GuestbookRouteAPI,
   auth: AuthRouteAPI,
+  stats: StatsRouteAPI,
 };
